506d40a3a8acd334335df2469cc7db42070b18dd
Rudie G <rauoldm@gmail.com>
2021-06-09 22:09:05 -0500

    Added licenses, prettied up template literal a bit

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-// TODO: Include packages needed for this application
-
 const inquirer = require('inquirer');
 const fs = require('fs');
 const util = require('util');
@@ -13,6 +11,40 @@ fs.mkdir(path.join(process.cwd(), 'README'), (err) => {
     console.log('Directory created successfully!');
     init();
 });
+
+const licenseArray = [
+    "Apache_v2",
+    "Apache License 2.0",
+    "https://opensource.org/licenses/Apache-2.0",
+    "AFL_v3",
+    "Academic Free License 3.0",
+    "https://opensource.org/licenses/AFL-3.0",
+    "GPL_v2",
+    "GNU General Public License version 2",
+    "https://opensource.org/licenses/GPL-2.0",
+    "GPL_v3",
+    "GNU General Public License version 3",
+    "https://opensource.org/licenses/GPL-3.0",
+    "ISC",
+    "ISC License",
+    "https://opensource.org/licenses/ISC",
+    "MIT",
+    "MIT License",
+    "https://opensource.org/licenses/MIT",
+    "MPL_v2",
+    "Mozilla Public License 2.0",
+    "https://opensource.org/licenses/MPL-2.0",
+    "BSD_2_Clause",
+    "2-clause BSD License",
+    "https://opensource.org/licenses/BSD-2-Clause",
+    "BSD_3_Clause",
+    "3-clause BSD License",
+    "https://opensource.org/licenses/BSD-3-Clause",
+    "Open",
+    "no",
+    ""
+]
+
 
 // Prompt the user with questions
 function promptUser() {
@@ -61,7 +93,24 @@ function promptUser() {
             type: "input",
             message: "Please enter your email: ",
             name: "email"
-        }
+        },
+        {
+            type: "list",
+            message: "Chose the appropriate license for this project: ",
+            choices: [
+                "Apache_v2",
+                "AFL_v3",
+                "GPL_v2",
+                "GPL_v3",
+                "ISC",
+                "MIT",
+                "MPL_v2",
+                "BSD_2_Clause",
+                "BSD_3_Clause",
+                "Open"
+            ],
+            name: "license",
+        },
     ])
 }
 
@@ -78,45 +127,61 @@ async function init() {
 }
 
 function writeREADME(data) {
-    return `
-  <h1>${data.title}</h1>
-    
-  
-  ## Description
----
-  
-  ${data.summary}
+    let index = licenseArray.indexOf(data.license)
+    let lName = licenseArray[index + 1]
+    let lLink = licenseArray[(index + 2)]
 
-    
-  ## Installation
+    return `# ${data.title}
+![badge](https://img.shields.io/badge/license-${data.license}-blue)<br />
+
+
+## Description 📝 
 ---
-  ${data.installation}
-  
-  ## Usage
+
+    📝 ${data.summary} 📝
+
+
+## Table of Contents  
+    - [Installation Instructions ⌨️](#installation-)
+    - [Usage 🖥️](#usage-️)
+    - [Contributors 📜](#contributors-)
+    - [Screenshots 📸](#screenshots-)
+    - [Contact 📠](#contact-)
+    - [License 📋](#license-️)
+
+## Installation ⌨️ 
 ---
-  ${data.usage}
+    ⌨️ ${data.installation} ⌨️
   
-  ## Contributors
+## Usage 🖥️ 
 ---
-  ${data.contributing}
+    🖥️ ${data.usage} 🖥️
   
-  ## Screenshots
+## Contributors 📜 
+---
+    📜 ${data.contributing} 📜
+  
+## Screenshot 📸 
 ---
   |  ${data.screenshotTitle}                              | 
-  |:------------------------------------------------------:|
   | <img alt="${data.screenshotAlt}" src="${data.screenshot}" width="150" height="150"> |
   <br />
   <br />
 
-  ## Contact
+## Contact 📠 
 ---
-  <br />
-  <br />
-  GitHub: [${data.username}](https://github.com/${data.username})
-  <br />
-  <br />
-  Email: ${data.email}
-  <br />
-  <br />
+<br />
+<br />
+GitHub: [${data.username}](https://github.com/${data.username})
+<br />
+<br />
+Email: ${data.email}
+<br />
+<br />
+
+## License ![badge](https://img.shields.io/badge/license-${data.license}-blue)
+---
+<br />
+This application is covered by <a href="${lLink}"> ${lName}</a> license.
 `
 }
